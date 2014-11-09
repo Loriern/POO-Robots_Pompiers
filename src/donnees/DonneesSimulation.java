e14a12d0d3772a22ccc12a87e93803d862133b93
medjo <ikhalo.o@hotmail.fr>
2014-11-09 21:24:19 +0100

    creation de DonnesSimulation

---
+++ src/donnees/DonneesSimulation.java
@@ -0,0 +1,14 @@
+package donnees;
+import java.util.*;
+
+public class DonneesSimulation{
+
+    private LinkedList<Incendie> listeIncendies;
+    private Carte carte;
+    private Robot[] pompiers;
+    
+    public DonneesSimulation(int nbLignes, int nbColonnes, int tailleCases){
+
+        this.carte = new Carte(nbLignes, nbColonnes, tailleCases);
+    }
+}
